beaf7ea45c64b405eca63b3c2127cf8fe9dd131e
bcturner777 <bturner@structured.com>
2021-01-04 17:04:10 -0800

    Added "Bearer" to printed output

--- openvuln_token.py
+++ openvuln_token.py
@@ -11,7 +11,7 @@ def get_auth_token():
     url = 'https://cloudsso.cisco.com/as/token.oauth2?grant_type=client_credentials'       # Endpoint URL
     resp = requests.post(url, auth=HTTPBasicAuth(KEY, CLIENT_SECRET))  # Make the POST Request
     access_token = resp.json()['access_token']    # Retrieve the Token from the returned JSON
-    print("Token Retrieved: {}".format(access_token))  # Print out the Token
+    print("Token Retrieved: Bearer {}".format(access_token))  # Print out the Token
     return access_token    # Create a return statement to send the token back for later use
 
 if __name__ == "__main__":
